3a8083dec2431be4d677d09cd7a2ef6644829841
angelDi0 <angel.diosdado1239@gmail.com>
2025-10-26 22:42:23 -0600

    Cambios en archivos con conflicto en el merge

--- app/src/main/java/com/example/proyectomovil/ui/AppViewProvider.kt
+++ app/src/main/java/com/example/proyectomovil/ui/AppViewProvider.kt
@@ -1,13 +1,17 @@
 package com.example.proyectomovil.ui
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.proyectomovil.ProyectoMovilApplication
+import com.example.proyectomovil.ui.ViewModel.Tarea.DetalleTareaViewModel
 import com.example.proyectomovil.ui.ViewModel.Tarea.TareaViewModel
 import com.example.proyectomovil.ui.ViewModel.home.HomeViewModel
+import com.example.proyectomovil.ui.ViewModel.nota.DetalleNotaViewModel
 import com.example.proyectomovil.ui.ViewModel.nota.NotaViewModel
+import com.example.proyectomovil.ui.ViewModel.settings.SettingsViewModel
 
 object AppViewModelProvider {
     val Factory = viewModelFactory {
@@ -16,12 +20,26 @@ object AppViewModelProvider {
         }
 
         initializer {
-            NotaViewModel(ProyectoMovilApplication().container.notaRepository)
+            NotaViewModel(this.createSavedStateHandle(),ProyectoMovilApplication().container.notaRepository)
         }
 
         // Falta inicializar el viewModel de tarea
         initializer {
-            TareaViewModel(ProyectoMovilApplication().container.tareaRepository)
+            TareaViewModel(this.createSavedStateHandle(), ProyectoMovilApplication().container.tareaRepository)
+        }
+
+        initializer {
+            DetalleTareaViewModel(this.createSavedStateHandle(),ProyectoMovilApplication().container.tareaRepository)
+        }
+
+        initializer {
+            DetalleNotaViewModel(this.createSavedStateHandle(), ProyectoMovilApplication().container.notaRepository)
+        }
+
+        initializer {
+            SettingsViewModel(
+                ProyectoMovilApplication().container.userPreferencesRepository
+            )
         }
 
     }
